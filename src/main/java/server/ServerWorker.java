4bcfaed81690e77e93df00bc0ef884f14530dabf
Mathias2860DK <mathias.enemark.poulsen@gmail.com>
2021-03-08 12:26:01 +0100

    handleSend method updated

--- src/main/java/server/ServerWorker.java
+++ src/main/java/server/ServerWorker.java
@@ -39,13 +39,13 @@ public class ServerWorker extends Thread {
 
         String line;
         //TODO: you must be connected to acces commands.
-        while ((line = reader.readLine()) != null){
-            String [] input = line.split("#");
-            if ("CONNECT".equals(input[0])){
+        while ((line = reader.readLine()) != null) {
+            String[] input = line.split("#");
+            if ("CONNECT".equals(input[0])) {
                 handleConnect(input[1]);
-            } else if ("SEND".equals(input[0])){
-                handleSend(input[1],input[2]);
-            } else if ("CLOSE".equalsIgnoreCase(input[0])){
+            } else if ("SEND".equals(input[0])) {
+                handleSend(input[1], input[2]);
+            } else if ("CLOSE".equalsIgnoreCase(input[0])) {
                 closeConnection();
             }
 
@@ -55,32 +55,42 @@ public class ServerWorker extends Thread {
 
     private void closeConnection() throws IOException {
         List<ServerWorker> serverWorkerList = server.getWorkerList();
-       server.removeWorker(this);
+        server.removeWorker(this);
 
         String msgAll = "";
         for (ServerWorker serverWorker : serverWorkerList) {
-            if (serverWorker.getLogin() != null){
-                msgAll += serverWorker.getLogin() +",";
+            if (serverWorker.getLogin() != null) {
+                msgAll += serverWorker.getLogin() + ",";
             }
             serverWorker.sendWhosisOnline();
         }
 
-clientSocket.close(); //should return CLOSE#0 --> TODO: Exeption
+        clientSocket.close(); //should return CLOSE#0 --> TODO: Exeption
     }
 
     private void handleSend(String sendTo, String text) throws IOException {
+        String error = "Brugeren findes ikke \n";
         List<ServerWorker> serverWorkerList = server.getWorkerList();
         String [] input = sendTo.split(",");
         for (ServerWorker serverWorker : serverWorkerList) {
+            if (sendTo.equals("*")){
+                String sendMsg = "MESSAGE#" + login + "#" + text + "\n";
+                serverWorker.sendToClients(sendMsg);
+            }
             for (int i = 0; i < input.length; i++) {
                 if (input[i].contains(serverWorker.getLogin())){
                     String sendMsg = "MESSAGE#" + login + "#" + text + "\n";
                     serverWorker.sendToClients(sendMsg);
-
+                    error = "";
                 }
+
             }
         }
+        outputStream.write(error.getBytes());
     }
+
+
+
 
     private void sendToClients(String msg) throws IOException {
         outputStream.write(msg.getBytes());
